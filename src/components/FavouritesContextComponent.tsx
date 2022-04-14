bf2c19db238702fceb53d35761997ef429a9be07
Rachel Miles <rachmiles@gmail.com>
2022-04-14 05:59:31 +0100

    Remove unused component

--- src/components/FavouritesContextComponent.tsx
+++
@@ -1,7 +0,0 @@
-import CharacterContainer from "./character_container";
-
-const FavouritesContextComponent: React.FC = () => {
-  return <CharacterContainer />;
-};
-
-export default FavouritesContextComponent;
